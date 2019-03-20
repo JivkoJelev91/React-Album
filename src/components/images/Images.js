5beb50826efefbcb814cd990704c2a7b011bf9c3
JivkoJelev91 <zhelev.zhivko.1991@gmail.com>
2019-03-20 16:36:05 +0200

    Add a favorite icon

--- src/components/images/Images.js
+++ src/components/images/Images.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { GridList, GridTile } from 'material-ui/GridList/';
+import CircularProgress from 'material-ui/CircularProgress';
 import IconButton from 'material-ui/IconButton';
 import ZoomIn from 'material-ui/svg-icons/action/zoom-in';
+import Favorite from 'material-ui/svg-icons/action/favorite';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
-import CircularProgress from 'material-ui/CircularProgress';
-import axios from 'axios';
 import { Pagination } from 'semantic-ui-react'
+import axios from 'axios';
 
 class Images extends Component {
 
@@ -45,19 +46,9 @@ class Images extends Component {
         this.callApi();
     }
 
-    handleOpen = (img) => {
-        this.setState({
-            open:true,
-            currentImg: img
-        })
-    }
+    handleOpen = img => this.setState({open:true,currentImg: img})
 
-    handleClose = () => {
-        this.setState({
-            open:false,
-        })
-    }
-
+    handleClose = () => this.setState({open:false});
 
     render() {
         const actions = [
@@ -68,26 +59,33 @@ class Images extends Component {
             return (
                 <div>
                     <GridList cols={4}>
+                        {/* All images of current page  */}
                         {this.state.images.map(img => (
                             <GridTile
                             title={img.title}
                             key={img.id}
                             actionIcon = {
                                 <IconButton onClick={() => this.handleOpen(img.url)}>
-                                    <ZoomIn color='#000'/>
+                                    <div className="icons">
+                                        <ZoomIn color='#000'/>
+                                        <Favorite />
+                                    </div>
                                 </IconButton>
                             }>
+                            
                                 <img src={img.url} alt={img.title}/>
                             </GridTile>
                         ))}
                     </GridList>
+                    {/*The modal with clicked img */}
                     <Dialog
                     actions={actions}
                     modal={false}
                     open={this.state.open}
                     onRequestClose={this.handleClose}>
-                        <img src={this.state.currentImg} alt="" style={{width: '100%'}} />
+                        <img src={this.state.currentImg} alt="img" style={{width: '100%'}} />
                     </Dialog>
+                    {/* All pages */}
                     <div id="paginator">
                         <Pagination 
                         defaultActivePage={this.state.defaultPage}
@@ -100,11 +98,11 @@ class Images extends Component {
         }else{
             return (
                 <div id="wrapper">
+                    {/* Loader */}
                     <div className="spinner">
                         <CircularProgress />
                     </div>
                 </div>
-             
             )
         }
     }
