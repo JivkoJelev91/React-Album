b11017202dedc79bfa0511d21cd617d91cfc9842
JivkoJelev91 <zhelev.zhivko.1991@gmail.com>
2019-03-25 17:31:41 +0200

    build

---
+++ build/precache-manifest.8004d2bf96f5afd00d7e165b3589d55e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ecc1051866a638b3bf54",
+    "url": "/static/js/main.5c86bff9.chunk.js"
+  },
+  {
+    "revision": "0ac5ed8fedabefa98ded",
+    "url": "/static/js/2.b9d9f18a.chunk.js"
+  },
+  {
+    "revision": "ecc1051866a638b3bf54",
+    "url": "/static/css/main.e8530ef9.chunk.css"
+  },
+  {
+    "revision": "d05128bf3ca9c9a28b8b7aa4d15db544",
+    "url": "/index.html"
+  }
+];
